27d80f84ac98980b0037d668a760370212b7275e
Carlos Ramirez <crbarrio@hotmail.com>
2026-04-14 10:48:33 +0200

    feat: move images array to separate data file for better organization

--- image-gallery/src/app/pages/gallery/gallery.ts
+++ image-gallery/src/app/pages/gallery/gallery.ts
@@ -1,50 +1,6 @@
 import { Component, signal } from '@angular/core';
-import { Image } from "../../interfaces/image.interface";
 import { GalleryComponent } from "../../components/gallery/gallery-component";
-
-const imagesArray: Image[] = [
-  {
-    id:'237',
-    src: 'https://picsum.photos/id/237/600',
-    alt: 'https://picsum.photos/id/237/600'
-  },
-  {
-    id:'238',
-    src: 'https://picsum.photos/id/238/600',
-    alt: 'https://picsum.photos/id/238/600'
-  },
-  {
-    id:'239',
-    src: 'https://picsum.photos/id/239/600',
-    alt: 'https://picsum.photos/id/239/600'
-  },
-  {
-    id:'240',
-    src: 'https://picsum.photos/id/240/600',
-    alt: 'https://picsum.photos/id/240/600'
-  },
-  {
-    id:'241',
-    src: 'https://picsum.photos/id/241/600',
-    alt: 'https://picsum.photos/id/241/600'
-  },
-  {
-    id:'242',
-    src: 'https://picsum.photos/id/242/600',
-    alt: 'https://picsum.photos/id/242/600'
-  },
-  {
-    id:'243',
-    src: 'https://picsum.photos/id/243/600',
-    alt: 'https://picsum.photos/id/243/600'
-  },
-  {
-    id:'244',
-    src: 'https://picsum.photos/id/244/600',
-    alt: 'https://picsum.photos/id/244/600'
-  },
-
-]
+import { imagesArray } from "./gallery.data";
 
 @Component({
   imports: [GalleryComponent],
